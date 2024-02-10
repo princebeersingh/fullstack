c8353ffa0894bbd05c041d26b0bc00f597e2faad
princebeersingh <princebeersingh@gmail.com>
2024-02-10 19:55:30 +0530

    setup for frontend and backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router ,Routes,Route} from "react-router-dom"
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+
+
+import "./Style/Navbar.scss"
+import "./Style/Footer.scss"
+import "./Style/Home.scss"
+import "./Style/About.scss"
+import "./Style/Contact.scss"
+import "./Style/Services.scss"
+import "./Style/Register.scss"
+import "./Style/Login.scss"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    <Navbar/>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/About' element={<About/>}/>
+    <Route path='/Login' element={<Login/>}/>
+    <Route path='/Contact' element={<Contact/>}/>
+    <Route path='/Logout' element={<Logout/>}/>
+    <Route path='/Register' element={<Register/>}/>
+    <Route path='/Services' element={<Services/>}/>
+    <Route path='*' element={<div><h1>Page Not Found 404!</h1></div>}/>
+    </Routes>
+    <Footer/>
+    </Router>
+
+    </>
   );
 }
 
